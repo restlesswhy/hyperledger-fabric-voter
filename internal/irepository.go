c33ae4e79a36e423648273c77475370ecfcd6fdf
g.generalov <g.generalov@x-tek.ru>
2022-05-17 17:43:10 +0200

    +

--- internal/irepository.go
+++ internal/irepository.go
@@ -6,4 +6,8 @@ type Repository interface {
 	CreateThread(threadID string, thread []byte) error
 	UpdateThread(threadID string, thread []byte) error
 	GetThread(threadID string) (*models.Thread, error)
+
+	CreateAnonThread(threadID string, thread []byte) error
+	UpdateThread(threadID string, thread []byte) error
+	GetThread(threadID string) (*models.Thread, error)
 }
